d476bb79db5c0325b1dca464d277d0d088e331e5
k190190 <k190190@nu.edu.pk>
2023-04-16 11:23:30 +0500

    first commit

---
+++ k190190.js
@@ -0,0 +1,27 @@
+const searchBox = document.querySelector('#searchBox');
+const projectList = document.querySelector('#projectList');
+
+searchBox.addEventListener('input', () => {
+  const searchTerm = searchBox.value.toLowerCase();
+  const projects = projectList.getElementsByTagName('li');
+
+  Array.from(projects).forEach((project) => {
+    const projectText = project.textContent.toLowerCase();
+
+    if (projectText.includes(searchTerm)) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+});
+
+function redirectToPage() {
+    window.location.href = "k190190_projects.html";
+  }
+  function redirectToHome() {
+    window.location.href = "k190190_homepage.html";
+  }
+  function redirectToEducation() {
+    window.location.href = "k190190_education.html";
+  }
